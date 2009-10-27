f78623c0126689507b85c03d564f793c83f186db
bloutix <bloutix@hotmail.fr>
2009-10-27 20:25:18 +0100

    Ajout miss combat
    Signed-off-by: bloutix <bloutix@hotmail.fr>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1027,6 +1027,8 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
 
 void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
 {
+	if (m_caster->hasUnitState(UNIT_STAT_DIED)) 
+        return;
     if (!unit || !effectMask)
         return;
 
