a3478808c6bc8d4505093c4a9936f8b1d906f4e5
MacWarrior <MacWarrior_world-of-boss@hotmail.Fr>
2010-02-22 17:27:16 +0100

    Correction du patch modifiant la limite de lvl. Le score de critique ne devrait pu etre buggé.

--- src/game/DBCStructure.h
+++ src/game/DBCStructure.h
@@ -873,7 +873,7 @@ struct GlyphSlotEntry
 };
 
 // All Gt* DBC store data for 100 levels, some by 100 per class/race
-#define GT_MAX_LEVEL    255
+#define GT_MAX_LEVEL    100
 
 struct GtBarberShopCostBaseEntry
 {
